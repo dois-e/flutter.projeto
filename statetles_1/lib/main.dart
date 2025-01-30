ccff04d38e2c9c864d20ba8886a088255400c838
ALLAN OLIVEIRA BALIEIRO <allan.4951@aluno.pr.senac.br>
2025-01-30 17:28:47 -0300

    atualizaçao

--- statetles_1/lib/main.dart
+++ statetles_1/lib/main.dart
@@ -18,6 +18,7 @@ class Aplicativo extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData.dark(),
+      debugShowCheckedModeBanner: false,
       //home e a primeira tela que carrega
       home: Scaffold(
         //appbar a barra superior
@@ -30,25 +31,67 @@ class Aplicativo extends StatelessWidget {
         body: Center(
           //organizar em colunas
           child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             //children serve para colocar varios widgets um atras do outro
             children: [
               //contador é uma caixa
               Container(
-                color: Color.fromARGB(255, 52, 52, 91),
+               
                 height: 200,
                 width: 200,
                 padding: EdgeInsets.only(top: 75),
+
+                decoration: BoxDecoration(
+                   color: Color.fromARGB(255, 16, 16, 228),
+                   borderRadius: BorderRadius.all(Radius.circular( 20)),
+                ),
                 child: Text(
                   "Ola mundo",
                   textAlign: TextAlign.center,
                   style: TextStyle(fontSize: 30),
                 ),
               ),
+              // widget de linha
+              Row(
+                //tipo de espaçamento
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+
+                //widget com filhos
+
+                children: [
+                  Icon(Icons.leaderboard, color: Colors.deepPurpleAccent, size:50,),
+                  Icon(Icons.person, color: Colors.deepPurple,size: 50,),
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  FloatingActionButton(
+                    child: Icon(Icons.add),
+                    onPressed:(){
+                        print("parabens,funcionou");
+
+                    },
+  
+              ),
+                ]
+              ),
             ],
           ),
+
         ),
-      ),
+bottomNavigationBar: BottomNavigationBar(items:[
+ BottomNavigationBarItem(icon:Icon(Icons.school),label:"escola" ),
+ BottomNavigationBarItem(icon:Icon(Icons.add_a_photo),label:"fotos" ),
+ BottomNavigationBarItem(icon:Icon(Icons.search),label:"buscar" ),
+]),
+
+
+
+
+),
+
+      
     );
   }
 }
